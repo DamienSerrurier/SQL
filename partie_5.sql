f5875d70a1a4213887cd999d3f8c5b9eeb09da00
DamienSerrurier <damien.ser@laposte.net>
2021-01-26 10:24:48 +0100

    Finalisation partie 5

--- partie_5.sql
+++ partie_5.sql
@@ -19,7 +19,7 @@ SELECT *
 -- Exercice 2: Dans la table languages, afficher toutes les versions de PHP.
 SELECT *
     -> FROM `languages`
-    -> WHERE `languages`= 'PHP';
+    -> WHERE `language`= 'PHP';
 +----+-----------+-------------+
 | id | language  | version     |
 +----+-----------+-------------+
@@ -32,7 +32,8 @@ SELECT *
 -- Exercice 3: Dans la table languages, afficher toutes les versions de PHP et de JavaScript.
 SELECT *
     -> FROM `languages`
-    -> WHERE `languages` <> 'HTML';
+    -> WHERE `language` <> 'HTML';
+    WHERE `language` = 'PHP' OR  `language` = 'JavaScript';
 +----+------------+-------------+
 | id | language   | version     |
 +----+------------+-------------+
@@ -45,3 +46,62 @@ SELECT *
 |  8 | PHP        | version 7   |
 +----+------------+-------------+
 7 rows in set (0.00 sec)
+
+-- Exercice 4: Dans la table languages, afficher toutes les lignes ayant pour id 3,5,7.
+SELECT *
+    -> FROM `languages`
+    -> WHERE `id`IN (3, 5, 7);
++----+------------+-------------+
+| id | language   | version     |
++----+------------+-------------+
+|  3 | PHP        | version 5.4 |
+|  5 | JavaScript | version 6   |
+|  7 | JavaScript | version 8   |
++----+------------+-------------+
+3 rows in set (0.00 sec)
+
+-- Exercice 5: Dans la table languages, afficher les deux première entrées de JavaScript.
+SELECT *
+    -> FROM `languages`
+    WHERE `language` = 'JavaScript'
+    LIMIT 2;
++----+------------+-----------+
+| id | language   | version   |
++----+------------+-----------+
+|  1 | JavaScript | version 5 |
+|  7 | JavaScript | version 8 |
++----+------------+-----------+
+2 rows in set (0.00 sec)
+
+-- Exercice 6: Dans la table languages, afficher toutes les lignes qui ne sont pas du PHP.
+SELECT *
+    -> FROM `languages`
+    -> WHERE `language` <> 'PHP';
++----+------------+-------------+
+| id | language   | version     |
++----+------------+-------------+
+|  1 | JavaScript | version 5   |
+|  4 | HTML       | version 5.1 |
+|  5 | JavaScript | version 6   |
+|  6 | JavaScript | version 7   |
+|  7 | JavaScript | version 8   |
++----+------------+-------------+
+5 rows in set (0.00 sec)
+
+-- Exercice 7: Dans la table languages, afficher toutes les données par ordre alphabétique.
+SELECT *
+    -> FROM `languages`
+    -> ORDER BY `language`;
++----+------------+-------------+
+| id | language   | version     |
++----+------------+-------------+
+|  4 | HTML       | version 5.1 |
+|  1 | JavaScript | version 5   |
+|  5 | JavaScript | version 6   |
+|  6 | JavaScript | version 7   |
+|  7 | JavaScript | version 8   |
+|  2 | PHP        | version 5.2 |
+|  3 | PHP        | version 5.4 |
+|  8 | PHP        | version 7   |
++----+------------+-------------+
+8 rows in set (0.00 sec)
